dd3c87fe4c47c336907ed0834b4ce07f3cf1a713
Héctor Añor de Maya <1910132@alu.murciaeduca.es>
2024-11-15 12:16:59 +0100

    Colisión con el suelo y gravedad
    El personaje ahora colisiona con el suelo y le afecta la gravedad. Tendré que cambiar el suelo porque está algo raro.

--- GalaxyDodgerNoRegrets/juego/juego.js
+++ GalaxyDodgerNoRegrets/juego/juego.js
@@ -5,8 +5,8 @@
     const TOPEABAJO = 570;
     const TOPEARRIBA = 0;
 
-    let x=225;        // posición inicial x del rectángulo
-    let y=225;      // posición inicial y del rectángulo
+    let x=50;        // posición inicial x del rectángulo
+    let y=466;      // posición inicial y del rectángulo
     let canvas;     // variable que referencia al elemento canvas del html
     let ctx;        // contexto de trabajo
     let idPersonaje, idSuelo, idAnimacionDerecha, idAnimacionCaDerecha, idAnimacionAgachadoDerecha, idAnimacionSaltoDerecha, idAnimacionIzquierda, idAnimacionCaIzquierda, idAnimacionAgachadoIzquierda, idAnimacionSaltoIzquierda;   // id de la animación
@@ -15,7 +15,10 @@
     let xParadoIzquierda = false;
     let xCaDerecha = false;
     let xCaIzquierda = false;
-    let yArriba = false;
+    let yArribaDerecha = false;
+    let yCaerDerecha = false;
+    let yArribaIzquierda = false;
+    let yCaerIzquierda = false;
     let yAbajoDerecha = false;
     let yAbajoIzquierda = false;
 
@@ -41,16 +44,34 @@
             [120, 312], [220, 312], [320, 312], [420, 312], [520, 312], [620, 312], // Animación caminando izquierda
 			[730, 12], [30, 112], [130, 112], // Animación agachado derecha
             [230, 112], [330, 112], [430, 112], // Animación agachado izquierda
-			[236, 102], // Animación salto ascendente derecha
-            [236, 102], // Animación salto descendente izquierda
-            [236, 102], // Animación salto ascendente derecha
-            [236, 102] // Animación salto descendente izquierda
+			[636, 102], // Animación salto ascendente derecha
+            [728, 102], // Animación salto descendente derecha
+            [8, 212], // Animación salto ascendente izquierda
+            [124, 212] // Animación salto descendente izquierda
         ]; // Posiciones del sprite donde recortar cada imagen
         this.velocidad = 7;
+        this.velocidadY = 0;
         this.tamañoX   = 26;
         this.tamañoY   = 74;
         this.enSuelo   = false;
+        this.gravedad = 2;
     }
+
+    DD.prototype.actualizarGravedad = function() {
+        // Aplicar gravedad
+        if (!this.enSuelo) {
+            this.velocidadY += this.gravedad;
+            this.y += this.velocidadY;
+        }
+    
+        // Verificar colisión con el suelo
+        if (this.y + this.tamañoY > miSuelo.posicionSueloY) {
+            this.y = miSuelo.posicionSueloY - this.tamañoY;
+            this.velocidadY = 0;
+            this.enSuelo = true;
+        }
+    }
+    
 
     DD.prototype.generaPosicionParado = function() {
         if (this.x > TOPEDERECHA) {
@@ -73,22 +94,28 @@
     }
 
     DD.prototype.generaPosicionArriba = function() {
-        this.y = this.y - this.velocidad;
-        if (this.y < TOPEARRIBA) {
-            this.y = TOPEARRIBA;   
+        if (this.enSuelo) {
+            this.velocidadY = -28;
+            this.enSuelo = false;
         }
-    }
+    }    
 
     DD.prototype.generaPosicionAbajo = function() {
-        this.y = this.y + this.velocidad;
-        if (this.y > TOPEABAJO) {
-            this.y = TOPEABAJO;   
+        if (this.y + this.tamañoY > miSuelo.posicionSueloY) {
+            this.y = miSuelo.posicionSueloY - this.tamañoY;
+            this.enSuelo = true;
+        } else {
+            this.enSuelo = false;
         }
     }
+    
 
     function pintaRectangulo() {
         // Borramos el canvas
-        ctx.clearRect(0, 0, 600, 600);      
+        ctx.clearRect(0, 0, 600, 600);
+        
+        miDD.actualizarGravedad();
+
         if (xParadoDerecha) {
             miDD.generaPosicionParado();
         }
@@ -98,7 +125,7 @@
         if (xCaIzquierda) {
             miDD.generaPosicionIzquierda();
         }
-        if (yArriba) {
+        if (yArribaDerecha) {
             miDD.generaPosicionArriba();
         }
         if (yAbajoDerecha) {
@@ -136,6 +163,11 @@
 			posicion = inicial + (posicion + 1) % framesCaminando;
             miDD.tamañoX = 58;
             miDD.tamañoY = 74;
+        } else if (yArribaDerecha) {
+            inicial = 24;
+            posicion = inicial + (posicion + 1) % 1;
+            miDD.tamañoX = 50;
+            miDD.tamañoY = 84;
 		} else if (yAbajoDerecha) {
 			inicial = 18;
 			posicion = inicial + (posicion + 1) % framesAgachado;
@@ -149,17 +181,12 @@
 			posicion = inicial + (posicion + 1) % framesCaminando;
             miDD.tamañoX = 58;
             miDD.tamañoY = 74;
-		} else if (yAbajoaIzquierda) {
-			inicial = 21;
-			posicion = inicial + (posicion + 1) % framesAgachado;
-            miDD.tamañoX = 36;
-            miDD.tamañoY = 74;
-        }
+		}
     }
 
     function activaMovimiento(evt) {
         switch (evt.keyCode) {
-            // Right arrow.
+            // Derecha
             case 39:
                 xParadoDerecha = false;
                 xCaDerecha = true;
@@ -167,7 +194,7 @@
 					idAnimacionCaDerecha = setInterval(DDanimaciones, 1000 / 7);
 				}
                 break;
-            // Left arrow.
+            // Izquierda
             case 37:
                 xParadoDerecha = false;
                 xParadoIzquierda = false;
@@ -176,15 +203,15 @@
 					idAnimacionCaIzquierda = setInterval(DDanimaciones, 1000 / 7);
 				}
                 break;
-            // Up arrow.
+            // Arriba
             case 38:
                 xParadoDerecha = false;
-                yArriba = true;
+                yArribaDerecha = true;
 				if (!idAnimacionSaltoDerecha) {
 					idAnimacionSaltoDerecha = setInterval(DDanimaciones, 1000 / 1);
 				}
                 break;
-            // Down arrow.
+            // Abajo
             case 40:
                 xParadoDerecha = false;
                 yAbajoDerecha = true;
@@ -197,16 +224,16 @@
 
     function desactivaMovimiento(evt) {
         switch (evt.keyCode) {
-            // Right arrow 
+            // Derecha
             case 39:
                 xCaDerecha = false;
 				if (!xCaDerecha) {
 					clearInterval(idAnimacionCaDerecha);
 					idAnimacionCaDerecha = null;
 				}
-                xParadoDerecha = !xCaIzquierda && !yArriba && !yAbajoDerecha;
+                xParadoDerecha = !xCaIzquierda && !yArribaDerecha && !yAbajoDerecha;
                 break;
-            // Left arrow.
+            // Izquierda
             case 37:
                 xCaIzquierda = false;
                 if (!xCaIzquierda) {
@@ -216,23 +243,23 @@
                 xParadoIzquierda = true;
                 xParadoDerecha = false;
                 break; 
-            // Up arrow.
+            // Arriba
             case 38:
-                yArriba = false;
-				if (!yArriba) {
+                yArribaDerecha = false;
+				if (!yArribaDerecha) {
 					clearInterval(idAnimacionSaltoDerecha);
 					idAnimacionSaltoDerecha = null;
 				}
                 xParadoDerecha = !xCaDerecha && !xCaIzquierda && !yAbajoDerecha;
                 break;
-            // Down arrow.
+            // Abajo
             case 40:
                 yAbajoDerecha = false;
 				if (!yAbajoDerecha) {
 					clearInterval(idAnimacionAgachadoDerecha);
 					idAnimacionAgachadoDerecha = null;
 				}
-                xParadoDerecha = !xCaDerecha && !xCaIzquierda && !yArriba;
+                xParadoDerecha = !xCaDerecha && !xCaIzquierda && !yArribaDerecha;
                 break;
         }
     }   
